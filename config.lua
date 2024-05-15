19160fc5220e88a08b50a5547b9ded175046456f
michaelplatt07 <michael.platt.07@gmail.com>
2024-05-14 21:17:37 -0600

    Using a percentage to create the window for previewers

--- config.lua
+++ config.lua
@@ -1,11 +1,13 @@
 local config = {}
 
+-- TODO(map) Make the height and width percentage configurable
 function config.init()
 	local rowHeight = vim.api.nvim_list_uis()[1].height
 	local rowWidth = vim.api.nvim_list_uis()[1].width
-	config.width = 100 -- TODO(map) Get the width and height to be calculated instead of hard values
-	config.height = 20
 
+	local current_win = vim.api.nvim_get_current_win()
+	config.width = vim.api.nvim_win_get_width(current_win) * 0.75
+	config.height = vim.api.nvim_win_get_height(current_win) * 0.75
 	config.row = (rowHeight - config.height) * 0.5
 	config.col = (rowWidth - config.width) * 0.5
 end
