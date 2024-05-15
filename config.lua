5aeda556e3c7b1578e5a9657fc17c3912f9f862a
michaelplatt07 <michael.platt.07@gmail.com>
2024-05-14 23:29:34 -0600

    Sizing windows side by side correctly for preview and reference list

--- config.lua
+++ config.lua
@@ -2,14 +2,13 @@ local config = {}
 
 -- TODO(map) Make the height and width percentage configurable
 function config.init()
-	local rowHeight = vim.api.nvim_list_uis()[1].height
-	local rowWidth = vim.api.nvim_list_uis()[1].width
-
 	local current_win = vim.api.nvim_get_current_win()
-	config.width = vim.api.nvim_win_get_width(current_win) * 0.75
-	config.height = vim.api.nvim_win_get_height(current_win) * 0.75
-	config.row = (rowHeight - config.height) * 0.5
-	config.col = (rowWidth - config.width) * 0.5
+	config.width = math.floor(vim.api.nvim_win_get_width(current_win) * 0.4)
+	config.height = math.floor(vim.api.nvim_win_get_height(current_win) * 0.4)
+	config.referencesWindowRow = vim.api.nvim_win_get_height(current_win) * 0.1
+	config.referencesWindowCol = vim.api.nvim_win_get_width(current_win) * 0.1
+	config.previewWindowRow = vim.api.nvim_win_get_height(current_win) * 0.1
+	config.previewWindowCol = vim.api.nvim_win_get_width(current_win) * 0.5
 end
 
 return config
