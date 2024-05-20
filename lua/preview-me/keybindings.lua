90c3a47cfac360eb07703887e9f40deb73df6ce9
michaelplatt07 <michael.platt.07@gmail.com>
2024-05-19 23:40:12 -0600

    Moving table values for module out of module delcaration

--- lua/preview-me/keybindings.lua
+++ lua/preview-me/keybindings.lua
@@ -1,10 +1,10 @@
-local keybindings = {
-	split_v = { "n", "<leader>vs", ':lua require("preview-me.windower").split_v_ref()<CR>', {} },
-	split_h = { "<leader>hs", ':lua require("preview-me.windower").split_h_ref()<CR>', {} },
-	new_tab = { "n", "<leader>t", ':lua require("preview-me.windower").open_in_new_tab()<CR>', {} },
-	curr_window = { "n", "<leader>o", ':lua require("preview-me.windower").open_in_curr_window()<CR>', {} },
-	quit = { "n", "q", ':lua require("preview-me.windower").close_window()<CR>', {} },
-}
+local keybindings = {}
+
+keybindings.split_v = { "n", "<leader>vs", ':lua require("preview-me.windower").split_v_ref()<CR>', {} }
+keybindings.split_h = { "<leader>hs", ':lua require("preview-me.windower").split_h_ref()<CR>', {} }
+keybindings.new_tab = { "n", "<leader>t", ':lua require("preview-me.windower").open_in_new_tab()<CR>', {} }
+keybindings.curr_window = { "n", "<leader>o", ':lua require("preview-me.windower").open_in_curr_window()<CR>', {} }
+keybindings.quit = { "n", "q", ':lua require("preview-me.windower").close_window()<CR>', {} }
 
 function keybindings.update_key_binding(custombind)
 	keybindings.curr_window[1] = custombind[1]
