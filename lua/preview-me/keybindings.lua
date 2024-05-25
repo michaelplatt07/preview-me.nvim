d06f407de556b9dbbb2fa7a61997e79237b41f5b
michaelplatt07 <michael.platt.07@gmail.com>
2024-05-25 15:05:01 -0600

    Adding missed binding value

--- lua/preview-me/keybindings.lua
+++ lua/preview-me/keybindings.lua
@@ -1,6 +1,6 @@
 local keybindings = {
 	split_v = { "n", "<leader>vs", ':lua require("preview-me.windower").split_v_ref()<CR>', {} },
-	split_h = { "<leader>hs", ':lua require("preview-me.windower").split_h_ref()<CR>', {} },
+	split_h = { "n", "<leader>hs", ':lua require("preview-me.windower").split_h_ref()<CR>', {} },
 	new_tab = { "n", "<leader>t", ':lua require("preview-me.windower").open_in_new_tab()<CR>', {} },
 	curr_window = { "n", "<leader>o", ':lua require("preview-me.windower").open_in_curr_window()<CR>', {} },
 	quit = { "n", "q", ':lua require("preview-me.windower").close_window()<CR>', {} },
