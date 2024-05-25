1e3026ef9c045adcfcae37006a226e5dcdfaeb16
michaelplatt07 <michael.platt.07@gmail.com>
2024-05-25 13:15:28 -0600

    Trying to print custombind value

--- lua/preview-me/keybindings.lua
+++ lua/preview-me/keybindings.lua
@@ -6,13 +6,8 @@ local keybindings = {
 	quit = { "n", "q", ':lua require("preview-me.windower").close_window()<CR>', {} },
 }
 
--- keybindings.split_v = { "n", "<leader>vs", ':lua require("preview-me.windower").split_v_ref()<CR>', {} }
--- keybindings.split_h = { "<leader>hs", ':lua require("preview-me.windower").split_h_ref()<CR>', {} }
--- keybindings.new_tab = { "n", "<leader>t", ':lua require("preview-me.windower").open_in_new_tab()<CR>', {} }
--- keybindings.curr_window = { "n", "<leader>o", ':lua require("preview-me.windower").open_in_curr_window()<CR>', {} }
--- keybindings.quit = { "n", "q", ':lua require("preview-me.windower").close_window()<CR>', {} }
-
 function keybindings.update_key_binding(custombind)
+	print("Custombind = ", custombind)
 	print("In update key binding, value = ", custombind[1])
 	print("In update key binding, cmd = ", custombind[2])
 	keybindings.curr_window[1] = custombind[1]
