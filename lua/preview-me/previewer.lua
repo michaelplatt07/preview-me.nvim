adc9977c01e30d4a426343ade5ef9e32dfadae79
michaelplatt07 <michael@lugia.lugia>
2026-05-01 10:16:14 -0600

    Removing more dead references

--- lua/preview-me/previewer.lua
+++ lua/preview-me/previewer.lua
@@ -20,25 +20,25 @@ function previewer.open_references()
 
 	-- Callback for when the cursor moves around in the buffer
 	vim.api.nvim_create_autocmd({ "CursorMoved" }, {
-		buffer = state.referenceBuf,
+		buffer = windower.referenceBuf,
 		callback = function()
 			if #state.lines > 0 then
 				state.update_selected_row()
-				vim.api.nvim_buf_set_lines(state.previewBuf, 0, #state.currentPreview, false, {})
-				vim.api.nvim_buf_set_lines(state.previewBuf, 0, #state.currentPreview, false, state.currentPreview)
+				vim.api.nvim_buf_set_lines(windower.previewBuf, 0, #state.currentPreview, false, {})
+				vim.api.nvim_buf_set_lines(windower.previewBuf, 0, #state.currentPreview, false, state.currentPreview)
 
 				-- Set the new currsor position based on the updated selected row
-				if config.lineBeforeCount == nil then
-					-- No config provided so we can just use the start line
-					vim.api.nvim_win_set_cursor(state.previewWin, { state.currentLineData.range.start.line + 1, 0 })
-				elseif config.lineBeforeCount > state.currentLineData.range.start.line then
+				if state.lineBeforeCount == nil then
+					-- No state provided so we can just use the start line
+					vim.api.nvim_win_set_cursor(windower.previewWin, { state.currentLineData.range.start.line + 1, 0 })
+				elseif state.lineBeforeCount > state.currentLineData.range.start.line then
 					-- Case when line before count is greater than start line which means we are displaying all the lines before
 					-- the line of interest so we can use the start line
-					vim.api.nvim_win_set_cursor(state.previewWin, { state.currentLineData.range.start.line + 1, 0 })
+					vim.api.nvim_win_set_cursor(windower.previewWin, { state.currentLineData.range.start.line + 1, 0 })
 				else
 					-- Case when line before count is less than start line which means we need to set the new relative position
 					-- based on the lineBeforeCount being the zero starting point
-					vim.api.nvim_win_set_cursor(state.previewWin, { config.lineBeforeCount + 1, 0 })
+					vim.api.nvim_win_set_cursor(windower.previewWin, { state.lineBeforeCount + 1, 0 })
 				end
 			end
 		end,
@@ -46,51 +46,35 @@ function previewer.open_references()
 
 	-- Populate the buffers with the reference information and previews
 	if #state.lines > 0 then
-		vim.api.nvim_buf_set_lines(state.referenceBuf, 0, 2, false, state.lines)
-		vim.api.nvim_buf_set_lines(state.previewBuf, 0, 6, false, state.currentPreview)
+		vim.api.nvim_buf_set_lines(windower.referenceBuf, 0, 2, false, state.lines)
+		vim.api.nvim_buf_set_lines(windower.previewBuf, 0, 6, false, state.currentPreview)
 	end
 
 	-- Set the filetype for the buffer
-	vim.api.nvim_buf_set_option(state.previewBuf, "filetype", util.get_file_type(state.currentLineData.uri))
+	vim.api.nvim_buf_set_option(windower.previewBuf, "filetype", util.get_file_type(state.currentLineData.uri))
 
 	-- Create the windows and set them in the state
-	state.referenceWin = windower.create_references_window(
-		state.referenceBuf,
-		true,
-		config.referencesWindowRow,
-		config.referencesWindowCol,
-		config.width,
-		config.height,
-		"References"
-	)
-	state.previewWin = windower.create_preview_window(
-		state.previewBuf,
-		false,
-		config.previewWindowRow,
-		config.previewWindowCol,
-		config.width,
-		config.height,
-		"Preview"
-	)
+	windower.create_references_window()
+	windower.create_preview_window()
 	-- Set the cursor to the correct line
-	if config.lineBeforeCount == nil then
-		-- No config provided so we can just use the start line
-		vim.api.nvim_win_set_cursor(state.previewWin, { state.currentLineData.range.start.line + 1, 0 })
-	elseif config.lineBeforeCount > state.currentLineData.range.start.line then
+	if state.lineBeforeCount == nil then
+		-- No state provided so we can just use the start line
+		vim.api.nvim_win_set_cursor(windower.previewWin, { state.currentLineData.range.start.line + 1, 0 })
+	elseif state.lineBeforeCount > state.currentLineData.range.start.line then
 		-- Case when line before count is greater than start line which means we are displaying all the lines before
 		-- the line of interest so we can use the start line
-		vim.api.nvim_win_set_cursor(state.previewWin, { state.currentLineData.range.start.line + 1, 0 })
+		vim.api.nvim_win_set_cursor(windower.previewWin, { state.currentLineData.range.start.line + 1, 0 })
 	else
 		-- Case when line before count is less than start line which means we need to set the new relative position
 		-- based on the lineBeforeCount being the zero starting point
-		vim.api.nvim_win_set_cursor(state.previewWin, { config.lineBeforeCount + 1, 0 })
+		vim.api.nvim_win_set_cursor(windower.previewWin, { state.lineBeforeCount + 1, 0 })
 	end
 
 	-- Initialize key bindings
-	keybindings.map_keys(state.referenceBuf)
+	keybindings.map_keys(windower.referenceBuf)
 
 	-- Set buffer to not modifiable
-	vim.api.nvim_buf_set_option(state.referenceBuf, "modifiable", false)
+	vim.api.nvim_buf_set_option(windower.referenceBuf, "modifiable", false)
 	-- TODO(map) Figure out how to make the preview buffer not be able to me modified
 end
 
@@ -184,38 +168,38 @@ end
 
 function previewer.move_cursor_down()
 	-- Get the preview window handle
-	local cursorPos = vim.api.nvim_win_get_cursor(state.previewWin)[1]
-	if cursorPos + 1 < #vim.api.nvim_buf_get_lines(state.previewBuf, 0, -1, true) then
-		vim.api.nvim_win_set_cursor(state.previewWin, { cursorPos + 1, 0 })
+	local cursorPos = vim.api.nvim_win_get_cursor(windower.previewWin)[1]
+	if cursorPos + 1 < #vim.api.nvim_buf_get_lines(windower.previewBuf, 0, -1, true) then
+		vim.api.nvim_win_set_cursor(windower.previewWin, { cursorPos + 1, 0 })
 	end
 end
 
 function previewer.page_cursor_down()
 	-- Get the preview window handle
-	local cursorPos = vim.api.nvim_win_get_cursor(state.previewWin)[1]
-	local lineCount = #vim.api.nvim_buf_get_lines(state.previewBuf, 0, -1, true)
+	local cursorPos = vim.api.nvim_win_get_cursor(windower.previewWin)[1]
+	local lineCount = #vim.api.nvim_buf_get_lines(windower.previewBuf, 0, -1, true)
 	if cursorPos + 10 < lineCount then
-		vim.api.nvim_win_set_cursor(state.previewWin, { cursorPos + 10, 0 })
+		vim.api.nvim_win_set_cursor(windower.previewWin, { cursorPos + 10, 0 })
 	else
-		vim.api.nvim_win_set_cursor(state.previewWin, { lineCount, 0 })
+		vim.api.nvim_win_set_cursor(windower.previewWin, { lineCount, 0 })
 	end
 end
 
 function previewer.move_cursor_up()
 	-- Get the preview window handle
-	local cursorPos = vim.api.nvim_win_get_cursor(state.previewWin)[1]
+	local cursorPos = vim.api.nvim_win_get_cursor(windower.previewWin)[1]
 	if cursorPos - 1 > 0 then
-		vim.api.nvim_win_set_cursor(state.previewWin, { cursorPos - 1, 0 })
+		vim.api.nvim_win_set_cursor(windower.previewWin, { cursorPos - 1, 0 })
 	end
 end
 
 function previewer.page_cursor_up()
 	-- Get the preview window handle
-	local cursorPos = vim.api.nvim_win_get_cursor(state.previewWin)[1]
+	local cursorPos = vim.api.nvim_win_get_cursor(windower.previewWin)[1]
 	if cursorPos - 10 > 0 then
-		vim.api.nvim_win_set_cursor(state.previewWin, { cursorPos - 10, 0 })
+		vim.api.nvim_win_set_cursor(windower.previewWin, { cursorPos - 10, 0 })
 	else
-		vim.api.nvim_win_set_cursor(state.previewWin, { 1, 0 })
+		vim.api.nvim_win_set_cursor(windower.previewWin, { 1, 0 })
 	end
 end
 
